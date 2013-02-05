4bdae0befee599c2200ec6a83024fcf2cdf671ab
funkdified <funkdified@gmail.com>
2013-02-06 11:47:40 +1300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandcamp::Application.config.secret_token = '8b3e1b2401db2332634fb546428f63a081b2255fc54cb4089fa4c5aec18bce2414d3e72ea8ee766facc72dd29514c87425512c96cc9523a1c8b1eee2d7a3b0db'
