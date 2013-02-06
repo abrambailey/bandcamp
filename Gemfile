28153166c083a59553be1847cce1aa510fcb886c
funkdified <funkdified@gmail.com>
2013-02-06 23:16:50 +1300

    added tag drop down

--- Gemfile
+++ Gemfile
@@ -1,6 +1,13 @@
 source 'https://rubygems.org'
 gem 'rails', '3.2.11'
 gem 'sqlite3'
+gem 'mechanize'
+gem 'nokogiri'
+
+gem "pry"
+gem "pry-debugger"
+gem 'chosen-rails'
+
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
+    require 'rubygems'
+    require 'mechanize'
+    require 'open-uri'
+
+    a = Mechanize.new
+    @tags = []
+    a.get("http://bandcamp.com/tags") do |page|
+      search_result = page.links.each do |link|
+        @tags << link.text.strip
+      end
+    end 
+    @tags.shift(2)
+
+    #binding.pry
   end
 end
